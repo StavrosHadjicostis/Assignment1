2e2f885db0d02b1b80b93af37d76f70d11ccc965
Stavros Hadjicostis <106922068+StavrosHadjicostis@users.noreply.github.com>
2024-10-23 20:10:33 +0300

    Add files via upload
    Declaration of main funtion and implementation of help function which prints messages of the command that the user inserts is in wrong format.

---
+++ net_recon.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+def help():
+    print("The command must include two arguments. A network interface name and an indicator for active or passive mode.\n")
+    print("Example 1: net_recon.py enp0s3 -i")
+    print("Example 2: net_recon.py enp0s3 -iface")
+
+
+
+
+
+
+
+def main():
+    print("Welcome to net_recon tool!!\n")
+
